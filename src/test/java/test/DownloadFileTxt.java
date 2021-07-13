bfda13c7b5c8f397a5183a60b22248844d1063d5
Alexander Terentev <33878295+winnerrr@users.noreply.github.com>
2021-07-13 08:09:52 +0300

    2

---
+++ src/test/java/test/DownloadFileTxt.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DownloadFileTxt {
+
+    @Test
+    void selenideDownloadText{
+
+    }
+}
